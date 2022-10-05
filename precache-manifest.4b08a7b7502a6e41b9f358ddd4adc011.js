130465a5ead02fd4f370b70e7a0ed542bf63af6a
Codewithjutt <usamaabdulrehman950@gmail.com>
2022-10-05 15:07:16 +0500

    Updates

---
+++ precache-manifest.4b08a7b7502a6e41b9f358ddd4adc011.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bbcd2ccd76339ac4fac7ab8c9d3ded",
+    "url": "/Codewithjutt/GroceryApp.git/index.html"
+  },
+  {
+    "revision": "69a2b23ba170843d08eb",
+    "url": "/Codewithjutt/GroceryApp.git/static/css/main.6ed62891.chunk.css"
+  },
+  {
+    "revision": "b87bb61cbce0aa3d9f57",
+    "url": "/Codewithjutt/GroceryApp.git/static/js/2.9df1a679.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Codewithjutt/GroceryApp.git/static/js/2.9df1a679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a2b23ba170843d08eb",
+    "url": "/Codewithjutt/GroceryApp.git/static/js/main.cee0aaab.chunk.js"
+  },
+  {
+    "revision": "f3f0f7b0727e944413b6",
+    "url": "/Codewithjutt/GroceryApp.git/static/js/runtime-main.3b87329b.js"
+  }
+]);
